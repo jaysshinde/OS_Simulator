29bee6b48153f459cb975561eb977124265f38b7
hlpr98 <hlpr98@gmail.com>
2018-03-17 00:53:41 +0530

    Initial commit

---
+++ OS Simulator/src/simulations/disk_management_File_allocation/File_allocationController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulations.disk_management_File_allocation;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author praveen
+ */
+public class File_allocationController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
